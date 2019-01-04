f874c0a00f888b861ad5249b34f232807bd95263
unknown <ithaqua16@gmail.com>
2019-01-04 09:10:35 +0100

    save

---
+++ accueil.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<title>Foundation | Welcome</title>
+<link rel="stylesheet" href="assets/css/foundation.css">
+</head>
+<body>
+
+<div class="top-bar">
+<div class="top-bar-left">
+<ul class="menu">
+<li class="menu-text">MINIMO</li>
+</ul>
+</div>
+<div class="top-bar-right">
+<ul class="menu">
+<li><a href="#">LIFESTYLE</a></li>
+<li><a href="#">PHOTODIARY</a></li>
+<li><a href="#">MUSIC</a></li>
+<li><a href="#">TRAVEL</a></li>
+</ul>
+</div>
+</div>
+
+<img src="assets/images/01_image_principale">
+
+<h3>The perfect weekend getaway</h3>
+<p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus. Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+
+<div class="grid-container">
+<div class="grid-x grid-margin-x" id="content">
+<div class="medium-6 cell">
+<div class="blog-post">
+<h3>More than just a music festival</h3>
+<img src="assets/images/02_festival">
+<p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+<div class="callout">
+<ul class="menu simple">
+<li><a href="#">Author: Mike Mikers</a></li>
+<li><a href="#">Comments: 3</a></li>
+</ul>
+</div>
+</div>
+
+<div class="blog-post">
+<h3>Life tastes better with coffee</h3>
+<img src="assets/images/03_caffee">
+<p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+<div class="callout">
+<ul class="menu simple">
+<li><a href="#">Author: Mike Mikers</a></li>
+<li><a href="#">Comments: 3</a></li>
+</ul>
+</div>
+</div>
+
+<div class="blog-post">
+<h3>American Dream</h3>
+<img src="assets/images/04_pont">
+<p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+<div class="callout">
+<ul class="menu simple">
+<li><a href="#">Author: Mike Mikers</a></li>
+<li><a href="#">Comments: 3</a></li>
+</ul>
+</div>
+</div>
+
+<div class="blog-post">
+<h3>A day exploring the Alps</h3>
+<img src="assets/images/05_montagnes">
+<p>Praesent id metus massa, ut blandit odio. Proin quis tortor orci. Etiam at risus et justo dignissim congue. Donec congue lacinia dui, a porttitor lectus condimentum laoreet. Nunc eu ullamcorper orci. Quisque eget odio ac lectus vestibulum faucibus eget in metus. In pellentesque faucibus vestibulum. Nulla at nulla justo, eget luctus.</p>
+<div class="callout">
+<ul class="menu simple">
+<li><a href="#">Author: Mike Mikers</a></li>
+<li><a href="#">Comments: 3</a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://dhbhdrzi4tiry.cloudfront.net/cdn/sites/foundation.js"></script>
+<script>
+      $(document).foundation();
+    </script>
+</body>
+</html>
