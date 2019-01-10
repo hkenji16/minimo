9a4a61eb42c0060ee075ce0606a6ebb7854ef4bd
unknown <ithaqua16@gmail.com>
2019-01-10 13:46:12 +0100

    save

--- views/accueil.php
+++ views/accueil.php
@@ -97,9 +97,6 @@ $posts->closeCursor();
 
 <?php require('views/template.php'); ?>
 
-<script src="js/vendor/jquery.js"></script>
-<script src="js/vendor/what-input.js"></script>
-<script src="js/vendor/foundation.js"></script>
-<script src="js/app.js"></script>
+
 </body>
 </html>
